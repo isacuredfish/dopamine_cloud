f45b208acf57207462bc960c4e1d6928a1a9b52a
dopamine <1471744992@qq.com>
2023-04-25 15:50:12 +0800

    payment001模块构建

---
+++ cloud-provider-payment001/src/main/java/com/dopamine/springcloud/PaymentMain001.java
@@ -0,0 +1,15 @@
+package com.dopamine.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenzi.ma
+ * @date 2023/04/25 15:46
+ **/
+@SpringBootApplication
+public class PaymentMain001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain001.class,args);
+    }
+}
